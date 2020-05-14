215e669915a9be4a52b6bb9c1be9a8d06491dd02
Encuestas-TEC <proyecto_encuestastec@hotmail.com>
2020-05-14 00:56:56 -0500

    Se creo repositorio y controlador de preguntas, sin metodos ni vistas.

---
+++ EncuestasITESRC/EncuestasITESRC/Repositories/PreguntasRepository.cs
@@ -0,0 +1,14 @@
+﻿using EncuestasITESRC.Areas.Administrador.Models;
+using EncuestasITESRC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncuestasITESRC.Repositories
+{
+    public class PreguntasRepository : Repository<Pregunta>
+    {
+
+    }
+}
