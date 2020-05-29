8927fa4ceabe043f2d7889593306cc9389527643
Encuestas-TEC <proyecto_encuestastec@hotmail.com>
2020-05-28 23:10:31 -0500

    Pruebas CRUD Preguntas

---
+++ EncuestasITESRC/EncuestasITESRC/Areas/Administrador/Models/ViewModels/DAPreguntasViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncuestasITESRC.Areas.Administrador.Models
+{
+    public class DAPreguntasViewModel
+    {
+        public int Id { get; set; }
+        public string Contenido { get; set; }
+        public int IdTipoRespuesta { get; set; }
+        public int IdEncuesta { get; set; }
+    }
+}
--- EncuestasITESRC/EncuestasITESRC/Repositories/PreguntasRepository.cs
+++ EncuestasITESRC/EncuestasITESRC/Repositories/PreguntasRepository.cs
@@ -9,6 +9,29 @@ namespace EncuestasITESRC.Repositories
 {
     public class PreguntasRepository : Repository<Pregunta>
     {
-
+        public void Insert(DAPreguntasViewModel vm)
+        {
+            Pregunta pregunta = new Pregunta
+            {
+                Id = vm.Id,
+                Contenido = vm.Contenido,
+                IdTipoRespuesta = vm.IdTipoRespuesta,
+                IdEncuesta = vm.IdEncuesta
+            };
+            Insert(pregunta);
+            vm.Id = pregunta.Id;
+        }
+        public void Update(DAPreguntasViewModel vm)
+        {
+            var pregunta = GetById(vm.Id);
+            if (pregunta != null)
+            {
+                pregunta.Id = vm.Id;
+                pregunta.Contenido = vm.Contenido;
+                pregunta.IdEncuesta = vm.IdEncuesta;
+                pregunta.IdTipoRespuesta = vm.IdTipoRespuesta;
+                Update(pregunta);
+            }
+        }
     }
 }
